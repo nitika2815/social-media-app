4030b1a85598afb74e50152fe23382d2bf186e60
nitika2815 <nitikakushwaha2807@gmail.com>
2025-07-28 15:20:55 +0530

    completed rightBar

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,9 +1,165 @@
-import "./rightBar.scss"
+import "./rightBar.scss";
 
 const RightBar = () => {
   return (
-    <div>RightBar</div>
-  )
-}
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggetions for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <span>Jone Deo</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <span>Jone Deo</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <p>
+                <span>Jone Deo</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <p>
+                <span>Jone Deo</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <p>
+                <span>Jone Deo</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <p>
+                <span>Jone Deo</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+        <span>Online Friends</span>
+        <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <div className="online"></div>
+              <span>Jone Deo</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <div className="online"></div>
+              <span>Jone Deo</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <div className="online"></div>
+                <span>Jone Deo</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <div className="online"></div>
+              <span>Jone Deo</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <div className="online"></div>
+              <span>Jone Deo</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <div className="online"></div>
+                <span>Jone Deo</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/700702502/photo/girlfriends-in-the-city.jpg?s=612x612&w=0&k=20&c=AM2hwTJCwyO4mDRU_mj97_2aAy55T5eGVu2T6gf6gwE="
+                alt="image"
+              />
+              <div className="online"></div>
+              <span>Jone Deo</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RightBar
+export default RightBar;
